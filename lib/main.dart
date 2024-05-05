ddf249e2a3e2e2fbfb8a2ee0f5b302afc1d604c0
Abhishek Saral <abhisheksaral17@gmail.com>
2024-05-05 09:41:25 -0700

    URL Strategy Set

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:revnotes_flutter_pocketbase/src/config/go_router_config.dart';
 import 'package:revnotes_flutter_pocketbase/src/config/theme_config.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(const MyApp());
 }
 
